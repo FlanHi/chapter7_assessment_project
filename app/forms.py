b786d060c1412fb37b7b71e03b693dac181c2ae0
FlanHi <imagebuyer985@gmail.com>
2023-03-18 14:08:38 +0300

    second commit

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
@@ -14,4 +14,5 @@ class ProForm(FlaskForm):
     """Product Form"""
     pro_name = StringField('Product Name', validators=[DataRequired(), Length(1, 16)])
     pro_desc = StringField('Product description', validators=[DataRequired()])
+    stock = SelectField(u'In Stock', choices=[('1', 'One'), ('2', 'Two'), ('3', 'Three')])
     submit = SubmitField('Add product')
